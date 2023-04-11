e8fd122bd91e44af010efe9e8a9d4847a150992a
kungyu.lee <kungyu.lee@wesang.com>
2023-04-11 23:49:56 +0900

    respond to multiple pings

--- app/main.py
+++ app/main.py
@@ -10,10 +10,14 @@ def main():
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
-    while True:
-        conn, addr = server_socket.accept() # wait for client
-        print(f"accepted connection from {addr}")
+    conn, addr = server_socket.accept() # wait for client
+    print(f"accepted connection from {addr}")
+
+    while data := conn.recv(1024):
+        print(f"received {data}")
+
         conn.send(b"+PONG\r\n")
+        print("sent pong response")
 
 
 if __name__ == "__main__":
