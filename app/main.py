17cc80ffd7feb8b6fb03e2742f5aeec3ffc821e6
kungyu.lee <kungyu.lee@wesang.com>
2023-04-11 22:59:33 +0900

    pong response

--- app/main.py
+++ app/main.py
@@ -7,9 +7,13 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
+    
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+
+    while True:
+        conn, addr = server_socket.accept() # wait for client
+        print(f"accepted connection from {addr}")
+        conn.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
