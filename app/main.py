3df4115e0f3daf42939b16c7019a9ece25e013e8
kungyu.lee <kungyu.lee@wesang.com>
2023-04-14 22:33:14 +0900

    try asyncio server

--- app/main.py
+++ app/main.py
@@ -1,21 +1,40 @@
-import socket
+import asyncio
 
 
-def main():
+async def _handle_read(reader: asyncio.StreamReader) -> None:
+    data = await reader.read(1024)
+    print(f"received {data}")
+
+
+async def _handle_write(writer: asyncio.StreamWriter) -> None:
+    writer.write(b"+PONG\r\n")
+    await writer.drain()
+    print("sent pong response")
+
+
+async def _handle_request(
+    reader: asyncio.StreamReader,
+    writer: asyncio.StreamWriter,
+) -> None:
+    print("start request")
+    await _handle_read(reader)
+    await _handle_write(writer)
+    print("end request\n")
+
+
+async def main():
     print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket = await asyncio.start_server(
+        _handle_request,
+        "localhost",
+        6379,
+        reuse_port=True,
+    )
 
-    while True:
-        conn, addr = server_socket.accept() # wait for client
-        print(f"accepted connection from {addr}")
-
-        while data := conn.recv(1024):
-            print(f"received {data}")
-
-            conn.send(b"+PONG\r\n")
-            print("sent pong response")
+    async with server_socket:
+        await server_socket.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
