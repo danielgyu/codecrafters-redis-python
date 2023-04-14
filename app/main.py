8159cc76568829cac967785f6932ae35f523130a
kungyu.lee <kungyu.lee@wesang.com>
2023-04-14 20:23:26 +0900

    keep server alive

--- app/main.py
+++ app/main.py
@@ -1,23 +1,20 @@
-# Uncomment this to pass the first stage
 import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
-    conn, addr = server_socket.accept() # wait for client
-    print(f"accepted connection from {addr}")
+    while True:
+        conn, addr = server_socket.accept() # wait for client
+        print(f"accepted connection from {addr}")
 
-    while data := conn.recv(1024):
-        print(f"received {data}")
+        while data := conn.recv(1024):
+            print(f"received {data}")
 
-        conn.send(b"+PONG\r\n")
-        print("sent pong response")
+            conn.send(b"+PONG\r\n")
+            print("sent pong response")
 
 
 if __name__ == "__main__":
